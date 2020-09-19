12d32cf55478f61d55328048d93ce380dd64847b
fey109 <fueryang961009@163.com>
2020-09-19 22:01:47 +0800

    项目描述

--- ey-code/src/main/java/com/ey/hc/ums/service/HcUserService.java
+++ ey-code/src/main/java/com/ey/hc/ums/service/HcUserService.java
@@ -1,6 +1,6 @@
 package com.ey.hc.ums.service;
 
-import com.ey.hc.ums.entity.UserInfo;
+import com.ey.hc.model.HcUser;
 
 import java.util.List;
 
@@ -10,7 +10,7 @@ import java.util.List;
  * @author makejava
  * @since 2020-06-17 23:18:25
  */
-public interface UserInfoService {
+public interface HcUserService {
 
     /**
      * 通过ID查询单条数据
@@ -18,7 +18,7 @@ public interface UserInfoService {
      * @param uId 主键
      * @return 实例对象
      */
-    UserInfo queryById(Long uId);
+    HcUser queryById(Long uId);
 
     /**
      * 查询多条数据
@@ -27,23 +27,23 @@ public interface UserInfoService {
      * @param limit 查询条数
      * @return 对象列表
      */
-    List<UserInfo> queryAllByLimit(int offset, int limit);
+    List<HcUser> queryAllByLimit(int offset, int limit);
 
     /**
      * 新增数据
      *
-     * @param userInfo 实例对象
+     * @param hcUser 实例对象
      * @return 实例对象
      */
-    UserInfo insert(UserInfo userInfo);
+    HcUser insert(HcUser hcUser);
 
     /**
      * 修改数据
      *
-     * @param userInfo 实例对象
+     * @param hcUser 实例对象
      * @return 实例对象
      */
-    UserInfo update(UserInfo userInfo);
+    HcUser update(HcUser hcUser);
 
     /**
      * 通过主键删除数据
